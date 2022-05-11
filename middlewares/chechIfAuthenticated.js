4f9e1f8e7be5605da6bf7df0d63ef09e6c3a782b
Shubham Baliyan <shubham_baliyan@epam.com>
2022-05-11 18:07:45 +0530

    added comments in auth midleware

--- middlewares/chechIfAuthenticated.js
+++ middlewares/chechIfAuthenticated.js
@@ -5,22 +5,26 @@ const AppError = require('../utils/appError');
 const checkIfAuthenticated = async (req, res, next) => {
   let token;
 
+  //check if req has authorization headers
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
   }
-
+  // if no token then send error
   if (!token) {
     return next(new AppError('You are not logged in', 401));
   }
-
+  //verfiy the token if present
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  // find if user is there or not
   const currentUser = await db.User.findByPk(decoded.id);
   if (!currentUser) {
     return next(new Error('The user no longer exists'));
   }
+
+  //every condition pass then call the next middleware and make the user available on the req
   req.user = currentUser;
   next();
 };
